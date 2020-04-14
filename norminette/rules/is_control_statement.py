5beda132e43308b9fed9fab1b53ac1abd8543f19
N01ch <noich@potiron>
2020-04-14 17:12:44 +0200

    parsing of else, else if, brackets

--- norminette/rules/is_control_statement.py
+++ norminette/rules/is_control_statement.py
@@ -16,7 +16,31 @@ class IsControlStatement(PrimaryRule):
         i = context.skip_ws(0)
         if context.check_token(i, cs_keywords) is False:
             return False, 0
-
+        token = context.peek_token(i)
+        if context.check_token(i, "ELSE") is True:
+            i += 1
+            while context.check_token(i, ["TAB", "SPACE"]) is True:
+                i += 1
+            if context.check_token(i, "NEWLINE") is True:
+                i = context.skip_ws(i)
+                if context.check_token(i, "LBRACE") is False:
+                    i -= 1
+                    while context.check_token(i, ["TAB", "SPACE"]) is True:
+                        i -= 1
+                    i += 1
+                    context.sub = context.scope.inner(ControlStructure)
+                    context.sub.multiline = False
+                    context.eol(i)
+                    return True, i
+                i += 1
+                context.sub = context.scope.inner(ControlStructure)
+                context.eol(i)
+                return True, i
+            elif context.check_token(i, "IF") is False:
+                print ("Parse error", context.peek_token(i))
+            else:
+                #return to basic handling
+                i += 1
         i += 1
         i = context.skip_ws(i)
         if context.check_token(i, "LPARENTHESIS") is False:
---
+++ norminette/tests/rules/ok_func_classic.c
@@ -0,0 +1,18 @@
+int main(void)
+{
+	while (i < 0)
+	{
+		return (val);
+	}
+	else if (toto)
+		if (che)
+			return (void);
+	else
+	{
+		return ("toto");
+	}
+	else if (woa)
+	{
+		print ('wolabrute');
+	}
+}
