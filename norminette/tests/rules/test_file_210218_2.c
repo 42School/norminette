ed46ccfc614fafb3e61b53bed475a0a877db164b
N01ch <noich@42network.org>
2021-02-18 13:03:32 +0100

    Added test_files

---
+++ norminette/tests/rules/test_file_210218_2.c
@@ -0,0 +1,9 @@
+#ifndef FT_EXAMPLE_H
+# define FT_EXAMPLE_H
+
+void	ft_foobar(
+			char really_long_variable_name_for_the_purposes_of_this_example,
+			char the_second_one_wouldent_fit_on_the_same_line
+			);
+
+#endif
