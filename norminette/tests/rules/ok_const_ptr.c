9b08736ed0ba73744cd827f657d170a6b8089705
ggjulio <juligonz@student.42.fr>
2021-03-04 17:38:29 +0100

    tests added

---
+++ norminette/tests/rules/ok_const_ptr.c
@@ -0,0 +1,13 @@
+int	func(void)
+{
+	int const							*var = 0;
+	const int							*var = 0;
+	t_obj const							*var = 0;
+	int *const *const *const *const		var = 0;
+	int **const							var = 0;
+	int const *const **const			var = 0;
+	const t_obj							*var = 0;
+	t_obj *const *const *const *const	var = 0;
+	t_obj **const						var = 0;
+	t_obj const *const **const			var = 0;
+}
