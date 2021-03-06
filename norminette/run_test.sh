ed87fccb3e20e9ef0cb8938e40a3727ed1bbfcab
ggjulio <juligonz@student.42.fr>
2021-03-06 15:31:06 +0100

    add timeout to job + fix run_tests.sh

--- norminette/run_test.sh
+++ norminette/run_test.sh
@@ -1,6 +1,10 @@
+#!/bin/env sh
+
+set -ex
+
 echo "Running lexer unit test:"
-python -m unittest discover tests/lexer/unit-tests/ "*.py"
+python3 -m unittest discover tests/lexer/unit-tests/ "*.py"
 echo "Running lexer test on files:"
-python -m  tests.lexer.files.file_token_test
-python -m tests.lexer.errors.tester
-python -m tests.rules.rule_tester
+python3 -m  tests.lexer.files.file_token_test
+python3 -m tests.lexer.errors.tester
+python3 -m tests.rules.rule_tester
