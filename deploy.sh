c2ec939688bdc9e903c48d1826831c187e4c783b
Тимур Марданов <timurmardanov97@gmail.com>
2021-03-09 16:31:06 +0300

    fixed pkg build (#85)
    Co-authored-by: Timur Mardanov <mardanov.timur@21-school.ru>
    Co-authored-by: Vincent <vin100.rey@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SUFFIX=""
+SUFFIX="V3"
 
 if [ "$#" -gt "0" ]
 then
@@ -8,28 +8,25 @@ then
 fi
 
 BUILD_DIR="build"
+BUNDLE_DIR="bundle"
 PKG_ROOT="$BUILD_DIR/pkgroot"
 PACKAGE_NAME="norminette$SUFFIX"
 DESCRIPTION="Norminette"
-VERSION=`cat norminette/version.py | cut -d'"' -f2`
-OUTFILE="norminette_$VERSION$SUFFIX.pkg"
+VERSION=`cat norminette/__init__.py | grep version | cut -d'"' -f2`
+OUTFILE="norminette_$SUFFIX_$VERSION.pkg"
 SUBDIRECTORY="apps/norminette"
 
-rm $OUTFILE
-
-rm -rf $BUILD_DIR
-mkdir $BUILD_DIR
+rm -rf $BUILD_DIR $BUNDLE_DIR
+mkdir $BUILD_DIR $BUNDLE_DIR
 python3 -m venv $BUILD_DIR/venv
 source $BUILD_DIR/venv/bin/activate
 python3 setup.py install
-sed -i '' 's#/Users/.*/venv#/usr/share/norminette/venv#' $BUILD_DIR/venv/bin/*
+sed -i '' 's#\#\!\/.*norminette.*/venv#\#\!/usr/local/share/norminetteV3/venv#' $BUILD_DIR/venv/bin/*
 deactivate
 
-mkdir -p $PKG_ROOT/usr/share/norminette
-mv $BUILD_DIR/venv $PKG_ROOT/usr/share/norminette/venv
+mkdir -p $PKG_ROOT/usr/local/share/$PACKAGE_NAME
+mv $BUILD_DIR/venv $PKG_ROOT/usr/local/share/$PACKAGE_NAME/venv
 
-pkgbuild --identifier $PACKAGE_NAME --version $VERSION --root $PKG_ROOT --install-location / $OUTFILE
-rm -rf $BUILD_DIR
-rm -rf dist
+pkgbuild --identifier $PACKAGE_NAME --version $VERSION --root $PKG_ROOT --install-location / $BUNDLE_DIR/$OUTFILE
 
-
+rm -rf $BUILD_DIR dist/
