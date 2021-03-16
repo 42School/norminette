2c3a4dc6099cec65abed86a4dc2835cbf928069c
N01ch <noich@42network.org>
2021-03-16 12:28:29 +0100

    Fixed indent check discrepancy (close #88)

--- norminette/tests/rules/random_test.c
+++ norminette/tests/rules/random_test.c
@@ -1,15 +1,6 @@
-static const t_quaternion	g_quat = (t_quaternion)
-{
-	1,
-	0,
-	0,
-	0
-};
-
-*g_result = (t_quaternion)
-{
-	1,
-	0,
-	0,
-	0
+static const t_quaternion	quat = {
+	.r = 1,
+	.i = 0,
+	.j = 0,
+	.k = 0
 };
