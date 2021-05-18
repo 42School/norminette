4995401f815ee14cad941f06eaef39f421081d1c
N01ch <noich@42network.org>
2021-05-18 09:21:27 +0200

    Close #139

--- norminette/rules/check_line_len.py
+++ norminette/rules/check_line_len.py
@@ -10,7 +10,11 @@ class CheckLineLen(Rule):
         """
         Lines must not be over 80 characters long
         """
+        i = 0
+        line_too_long = {}
         for tkn in context.tokens[: context.tkn_scope]:
-            if tkn.type == "NEWLINE" and tkn.pos[1] > 81:
+            if tkn.pos[1] > 81 and tkn.pos[0] not in line_too_long:
                 context.new_error("LINE_TOO_LONG", tkn)
+                line_too_long[tkn.pos[0]] = True
+            i += 1
         return False, 0
