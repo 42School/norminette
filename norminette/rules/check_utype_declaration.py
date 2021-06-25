5613462715b0e6a5958976d82fb9f24ac877f23a
N01ch <noich@potiron>
2021-06-25 08:48:52 +0200

    Close #166, fix for parsing typedef function pointer

--- norminette/rules/check_utype_declaration.py
+++ norminette/rules/check_utype_declaration.py
@@ -49,7 +49,7 @@ class CheckUtypeDeclaration(Rule):
                 pass
             if context.check_token(i, ["LPARENTHESIS"]) is True:
                 val, tmp = context.parenthesis_contain(i)
-                if val == None:
+                if val == None or val == "cast":
                     i = tmp
             if context.check_token(i, utypes) is True:
                 utype = context.peek_token(i)
@@ -75,6 +75,7 @@ class CheckUtypeDeclaration(Rule):
                 i = context.skip_nest(i)
             i += 1
         check = -1
+        #print (ids, utype, contain_full_def)
         if is_td == True and len(ids) < 2 and utype != None:
             context.new_error("MISSING_TYPEDEF_ID", context.peek_token(0))
             return False, 0
