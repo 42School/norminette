820cc427df3f94aaf21fbdd956d8db022c67ee16
N01ch <noich@potiron>
2021-09-23 14:09:28 +0200

    Close #226 - goto checked

--- norminette/rules/check_label.py
+++ norminette/rules/check_label.py
@@ -4,11 +4,18 @@ from norminette.rules import Rule
 class CheckLabel(Rule):
     def __init__(self):
         super().__init__()
-        self.depends_on = ["IsLabel"]
+        self.depends_on = []
 
     def run(self, context):
         """
         Goto and labels are forbidden
         """
-        context.new_error("LABEL_FBIDDEN", context.peek_token(0))
+        i = 0
+        if context.scope.name != "Function":
+            return False, 0
+        while i < context.tkn_scope:
+            if context.check_token(i, ["GOTO", "COLON"]):
+                context.new_error("LABEL_FBIDDEN", context.peek_token(0))
+                return False, 0
+            i += 1
         return False, 0
