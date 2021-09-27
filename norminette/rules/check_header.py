ad3102bc15d3b1b8120991ea0f02e9bc703ff2b0
N01ch <noich@potiron>
2021-09-27 09:02:59 +0200

    Header is now a real error

--- norminette/rules/check_header.py
+++ norminette/rules/check_header.py
@@ -8,7 +8,8 @@ class CheckHeader(Rule):
 
     def parse_header(self, context):
         if context.check_token(0, "MULT_COMMENT") is False:
-            print ("Missing or invalid header. Header are being reintroduced as a mandatory part of your files. This is not yet an error.")
+            # print ("Missing or invalid header. Header are being reintroduced as a mandatory part of your files. This is not yet an error.")
+            context.new_error("INVALID_HEADER", context.peek_token(0))
             context.header_parsed = True
             return
         context.header += context.peek_token(0).value + '\n'
@@ -22,8 +23,8 @@ class CheckHeader(Rule):
         #correct_header_no_nl = re.match(val_no_check_nl, context.header)
         correct_header_no_nl = regex.search(context.header)
         if correct_header_no_nl is None:
-            print ("Missing or invalid header. Header are being reintroduced as a mandatory part of your files. This is not yet an error.")
-            #context.new_error("INVALID_HEADER", context.peek_token(0))
+            # print ("Missing or invalid header. Header are being reintroduced as a mandatory part of your files. This is not yet an error.")
+            context.new_error("INVALID_HEADER", context.peek_token(0))
         #else:
         #    print (correct_header.group(1,2,3,4,5,6))
 
@@ -40,6 +41,6 @@ class CheckHeader(Rule):
             self.check_header(context)
             context.header_parsed = True
         elif context.header_started == False and context.header_parsed == False and context.history[-1] != "IsComment":
-            #context.new_error("INVALID_HEADER", context.peek_token(0))
-            print ("Missing or invalid header. Header are being reintroduced as a mandatory part of your files. This is not yet an error.")
+            context.new_error("INVALID_HEADER", context.peek_token(0))
+            # print ("Missing or invalid header. Header are being reintroduced as a mandatory part of your files. This is not yet an error.")
             context.header_parsed = True
