1edad353754bad091bcb67b89a88101cb106b22f
NiumXp <formigacomcaimbra@gmail.com>
2023-06-27 20:57:58 -0300

    fix empty line when checking a label

--- norminette/rules/is_label.py
+++ norminette/rules/is_label.py
@@ -18,7 +18,7 @@ class IsLabel(PrimaryRule):
             return False, 0
         i = context.skip_ws(i + 1)  # +1 to skip the colon
         if context.check_token(i, "NEWLINE"):
-            return True, i
+            return True, i+1
         while context.peek_token(i) and context.check_token(i, "SEMI_COLON") is False:
             i += 1
         i = context.eol(i + 1)  # +1 to skip the semi-colon
