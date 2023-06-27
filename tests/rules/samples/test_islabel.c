e5200834a372f9f0e715fa646972d669b09f8436
NiumXp <formigacomcaimbra@gmail.com>
2023-06-27 20:48:33 -0300

    add tests for label and goto

---
+++ tests/rules/samples/test_islabel.c
@@ -0,0 +1,32 @@
+int	main(void)
+{
+	dummy(4);
+	return (0);
+}
+
+void	dummy(int n)
+{
+	if (n == 4)
+		goto _4;
+	if (n == 2) {
+		goto _2;
+	}
+	goto end;
+	_4:
+	{
+		write(1, "4", 1);
+	}
+	_2:
+		write(1, "2", 1);
+end:
+	write(1, "\n", 1);
+bla: return;
+blo: printf(
+	"NiumXp :D")
+	 ; 
+ble:(
+	printf(
+	"NiumXp :D"
+	)
+	)
+;}
