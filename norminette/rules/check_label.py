c287a8c5f576e8c4acdd829743df6bbc8f927c06
NiumXp <formigacomcaimbra@gmail.com>
2023-06-29 04:58:19 -0300

    fix CheckLabel rule (#391)
    * add to CheckLabel check in nested function brackets and imporve search pattern for goto and label
    
    * remove wrong error when checking a ternary operator
    
    * add to CheckLabel check in nested function brackets and imporve search pattern for goto and label
    
    * remove wrong error when checking a ternary operator
    
    * add GOTO_FBIDDEN error
    
    * add tests for #390

--- norminette/rules/check_label.py
+++ norminette/rules/check_label.py
@@ -11,11 +11,15 @@ class CheckLabel(Rule):
         Goto and labels are forbidden
         """
         i = 0
-        if context.scope.name != "Function":
+        if context.scope.name not in ("Function", "ControlStructure"):
             return False, 0
-        while i < context.tkn_scope:
-            if context.check_token(i, ["GOTO", "COLON"]):
-                context.new_error("LABEL_FBIDDEN", context.peek_token(0))
-                return False, 0
-            i += 1
+        i = context.skip_ws(i)
+        if context.check_token(i, "GOTO"):
+            context.new_error("GOTO_FBIDDEN", context.peek_token(0))
+            return False, 0
+        if context.check_token(i, "IDENTIFIER") is False:
+            return False, 0
+        i = context.skip_ws(i + 1)
+        if context.check_token(i, "COLON"):
+            context.new_error("LABEL_FBIDDEN", context.peek_token(0))
         return False, 0
