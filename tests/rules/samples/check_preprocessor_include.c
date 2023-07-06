26bf2bd9b9aafa979f9e2d72433cf648a4b25ee7
NiumXp <formigacomcaimbra@gmail.com>
2023-07-05 21:49:23 -0300

    add tests for all CheckPreprocessor* rules

---
+++ tests/rules/samples/check_preprocessor_include.c
@@ -0,0 +1,12 @@
+#include "ok.h"
+#include "error.c"
+#include "error"
+
+
+void	main(void);
+
+#include "not in start.h"
+
+#if 1
+# include "ok but not ok.h"
+#endif
---
+++ tests/rules/samples/check_preprocessor_protection.h
@@ -0,0 +1,16 @@
+#define X 1
+
+#ifndef check_preprocessor_protection_h
+# define CHECK_PREPROCESSOR_PROTECTION_H
+#
+#endif
+
+#define Y 2
+
+#ifndef check_preprocessor_protection_h
+# define CHECK_PREPROCESSOR_PROTECTION_H
+#endif
+
+#ifndef check_preprocessor_protection_h
+# define CHECK_PREPROCESSOR_PROTECTION_H
+#endif
